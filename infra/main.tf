64bba4d365c0ee0baa27d0c835f00022d8bb8312
vinicius <vini.ads13@outlook.com>
2023-04-23 22:27:27 -0300

    :pencil: project finished

--- infra/main.tf
+++ infra/main.tf
@@ -43,11 +43,12 @@ resource "aws_autoscaling_group" "grupo" {
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
+  target_group_arns = var.producao ? [aws_lb_target_group.loadBalancerTarget[0].arn] : []
+
   launch_template {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
-  target_group_arns = [aws_lb_target_group.loadBalancerTarget.arn]
 }
 
 resource "aws_default_subnet" "subnet_1" {
@@ -61,6 +62,7 @@ resource "aws_default_subnet" "subnet_2" {
 resource "aws_lb" "loadBalancer" {
   internal = false
   subnets = [ aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id ]
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_default_vpc" "default" {
@@ -71,15 +73,17 @@ resource "aws_lb_target_group" "loadBalancerTarget" {
   port = "8000"
   protocol = "HTTP"
   vpc_id = aws_default_vpc.default.id
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_lb_listener" "entradaLoadBalancer" {
-  load_balancer_arn = aws_lb.loadBalancer.arn
+  load_balancer_arn = aws_lb.loadBalancer[0].arn
   port = "8000"
   protocol = "HTTP"
+  count = var.producao ? 1 : 0
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.loadBalancerTarget.arn
+    target_group_arn = aws_lb_target_group.loadBalancerTarget[0].arn
   }
 }
 
@@ -87,6 +91,7 @@ resource "aws_autoscaling_policy" "escala-producao" {
  name = "terraform-escala"
  autoscaling_group_name = var.nomeGrupo
  policy_type = "TargetTrackingScaling"
+ count = var.producao ? 1 : 0
  target_tracking_configuration {
     predefined_metric_specification {
 	predefined_metric_type = "ASGAverageCPUUtilization"	
